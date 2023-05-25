ea27a1fba05b37968fea04560ac8975304baf812
LudmilaMSoares <134635313+LudmilaMSoares@users.noreply.github.com>
2023-05-25 13:58:26 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,35 @@
+var numeroAleatorio = Math.floor(Math.random() * 100) + 1;
+
+		tentativas = 0;
+		acertou = false;
+		pontuacao = 0;
+
+		function verificar() {
+			const numeroDigitado = parseInt(document.getElementById("numero").value);
+
+			if (numeroDigitado < 1 || numeroDigitado > 100 || isNaN(numeroDigitado)) {
+				return;
+			}
+
+			tentativas++;
+
+			if (numeroDigitado === numeroAleatorio) {
+				acertou = true;
+				pontuacao = 100 - (tentativas - 1) * 10;
+				var labelNum = document.getElementById("labelNum");
+				labelNum.innerHTML= document.getElementById("resultado").innerHTML = "Parabéns! Você acertou o número em "+tentativas+ "tentativas!";
+				document.getElementById("pontuacao").innerHTML = "Sua pontuação: "+pontuacao;
+			} else if (numeroDigitado < numeroAleatorio) {
+				document.getElementById("resultado").innerHTML = "Tente um número maior que "+numeroDigitado;
+			} else {
+				document.getElementById("resultado").innerHTML = "Tente um número menor que " +numeroDigitado;
+			}
+		}
+
+		function reiniciar() {
+			tentativas = 0;
+			acertou = false;
+			pontuacao = 0;
+			var labelNum = document.getElementById("labelNum");
+				labelNum.innerHTML=  document.getElementById("numero").value = ""; document.getElementById("resultado").innerHTML = ""; document.getElementById("pontuacao").innerHTML = "";
+		}
